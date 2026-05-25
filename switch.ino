e4ed98298504d3b34c23996a9af6099c0a2b26cc
cells-oss <open-source-version@proton.me>
2026-05-25 20:52:19 +0300

    added 2 more buttons

--- switch.ino
+++ switch.ino
@@ -1,19 +1,37 @@
-const int switchPin = 2;
+const int switchPin1 = 2;
+const int switchPin2 = 4;
+const int switchPin3 = 7;
 int lastState = HIGH;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(switchPin, INPUT);
+  pinMode(switchPin1, INPUT);
+  pinMode(switchPin2, INPUT);
+  pinMode(switchPin3, INPUT);
 }
 
 void loop() {
-  int currentState = digitalRead(switchPin);
-  
-  if (currentState == LOW && lastState == HIGH) {
-    Serial.println("PRESSED");
+  int currentState1 = digitalRead(switchPin1);
+  int currentState2 = digitalRead(switchPin2);
+  int currentState3 = digitalRead(switchPin3);
+
+  if (currentState1 == LOW && lastState == HIGH) {
+    Serial.println("PRESSED1");
+    delay(50);
+  }
+  if (currentState2 == LOW && lastState == HIGH) {
+    Serial.println("PRESSED2");
+    delay(50);
+  }
+  if (currentState3 == LOW && lastState == HIGH) {
+    Serial.println("PRESSED3");
     delay(50);
   }
   
-  lastState = currentState;
+  lastState = currentState1;
+  delay(20);
+  lastState = currentState2;
+  delay(20);
+  lastState = currentState3;
   delay(20);
 }
